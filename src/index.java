08a958d62d0f1185bca5cb90744b5630852ce835
DinhHieu96 <truongdinhhieu1496@gamil.com>
2019-07-29 22:18:56 +0700

    khai báo

---
+++ src/index.java
@@ -0,0 +1,16 @@
+public class index {
+    public static void main(String[] args) {
+        int i=10;
+        float f=20.5f;
+        double d=20.5;
+        boolean c=true;
+        char e='a';
+        String s="Ha Noi";
+        System.out.println("i= "+i);
+        System.out.println("f= "+f);
+        System.out.println("d= "+d);
+        System.out.println("c= "+c);
+        System.out.println("e= "+e);
+        System.out.println("s= "+s);
+    }
+}
